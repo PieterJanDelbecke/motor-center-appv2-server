ea13466cb6143b9df4d4b812edd3b27949fe11a2
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2023-04-27 16:22:47 +1000

    setup of express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import express from "express";
+const app = express();
+const router = express.Router();
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
